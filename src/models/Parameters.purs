ddf7893abe526e2ccc11fb192d790b13558d93d1
Jim Dalrymple <jamesdalrymple@protonmail.com>
2020-03-14 19:28:51 +1100

    Changed variadic behavior

--- src/models/Parameters.purs
+++ src/models/Parameters.purs
@@ -1,17 +1,17 @@
 module Models.Parameters where
 
-import Prelude (class Eq, class Show, eq, show)
+import Prelude (class Eq, class Show, (<>), (>=), (==), eq, show)
 
-data ParameterCount = Variadic | Parameters Int
+data ParameterCount = Variadic Int | Fixed Int
 
 eqParameterCountImpl ∷ ParameterCount → ParameterCount → Boolean
-eqParameterCountImpl  Variadic       _             = true
-eqParameterCountImpl  _              Variadic      = true
-eqParameterCountImpl (Parameters n) (Parameters m) = eq n m
+eqParameterCountImpl (Fixed n) (Variadic m) = n >= m
+eqParameterCountImpl (Fixed n) (Fixed    m) = n == m
+eqParameterCountImpl (Variadic _)        _  = true
 
 instance parameterCountEq ∷ Eq ParameterCount where
   eq = eqParameterCountImpl
 
 instance parameterCountShow ∷ Show ParameterCount where
-  show  Variadic      = "∞"
-  show (Parameters n) = show n
+  show (Variadic  n) = show n <> "..∞"
+  show (Fixed     n) = show n
--- src/models/Procedure.purs
+++ src/models/Procedure.purs
@@ -1,10 +1,25 @@
 module Models.Procedure where
 
-import Models.Bytes (Bytes)
-import Models.Parameters (ParameterCount)
+import Models.Bytes (Bytes(..))
+import Models.Parameters (ParameterCount(..))
 
-type Procedure = {
+type Signature = {
   name ∷ String,
-  parameters ∷ ParameterCount,
-  bytes ∷ Bytes
+  args ∷ ParameterCount,
+  bytes ∷ Array Bytes
 }
+
+procedures ∷ Array Signature
+procedures = [
+  {name: "f64", args: Fixed 1, bytes: [Bytes [68], Slot]},
+  {name: "+", args: Variadic 2, bytes: [Bytes [160], Slot, Slot]},
+  {name: "-", args: Variadic 2, bytes: [Bytes [161], Slot, Slot]},
+  {name: "*", args: Variadic 2, bytes: [Bytes [162], Slot, Slot]},
+  {name: "/", args: Variadic 2, bytes: [Bytes [163], Slot, Slot]},
+  {name: "=", args: Variadic 2, bytes: [Bytes [97], Slot, Slot]},
+  {name: "!=", args: Variadic 2, bytes: [Bytes [98], Slot, Slot]},
+  {name: "<", args: Variadic 2, bytes: [Bytes [99], Slot, Slot]},
+  {name: ">", args: Variadic 2, bytes: [Bytes [100], Slot, Slot]},
+  {name: "<=", args: Variadic 2, bytes: [Bytes [101], Slot, Slot]},
+  {name: ">=", args: Variadic 2, bytes: [Bytes [102], Slot, Slot]}
+] 
