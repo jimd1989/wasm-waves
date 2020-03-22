a714735155bcf1450ec6b652bd84135b3806f2ec
Jim Dalrymple <jamesdalrymple@protonmail.com>
2020-03-22 16:13:13 +1100

    Added ternary operator

--- src/models/Signatures.purs
+++ src/models/Signatures.purs
@@ -30,10 +30,11 @@ signatures = [
   {name: "-", args: Variadic 2, slots: [], body: [[161]]},
   {name: "*", args: Variadic 2, slots: [], body: [[162]]},
   {name: "/", args: Variadic 2, slots: [], body: [[163]]},
-  {name: "=", args: Fixed 2, slots: [0, 1], body: [e, e, [97]]},
-  {name: "!=", args: Fixed 2, slots: [0, 1], body: [e, e, [98]]},
-  {name: "<", args: Fixed 2, slots: [0, 1], body: [e, e, [99]]},
-  {name: ">", args: Fixed 2, slots: [0, 1], body: [e, e, [100]]},
-  {name: "<=", args: Fixed 2, slots: [0, 1], body: [e, e, [101]]},
-  {name: ">=", args: Fixed 2, slots: [0, 1], body: [e, e, [102]]}
+  {name: "=", args: Fixed 2, slots: [0,1], body: [e,e,[97]]},
+  {name: "!=", args: Fixed 2, slots: [0,1], body: [e,e,[98]]},
+  {name: "<", args: Fixed 2, slots: [0,1], body: [e,e,[99]]},
+  {name: ">", args: Fixed 2, slots: [0,1], body: [e,e,[100]]},
+  {name: "<=", args: Fixed 2, slots: [0,1], body: [e,e,[101]]},
+  {name: ">=", args: Fixed 2, slots: [0,1], body: [e,e,[102]]},
+  {name: "?", args: Fixed 3, slots: [0,2,4], body: [e,[4,124],e,[5],e,[11]]} 
 ]
